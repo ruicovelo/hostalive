2e0d409d3fce8b3b002fb1e9718d78ee1cd21829
Rui Covelo <rui.covelo@gmail.com>
2013-07-16 10:31:27 +0100

    Added command line parsing to python version

--- hostalive.py
+++ hostalive.py
@@ -3,51 +3,62 @@
 import socket
 import sys
 import time
+import argparse
+
+RETRIES=100
+TIMEOUT_SECONDS=10
+SLEEP_SECONDS=30
+
+parser = argparse.ArgumentParser(description='Check if host/service is alive.')
+parser.add_argument('hostname',metavar='hostname',type=str,nargs=1,help='name of server (short or FQDN)')
+parser.add_argument('port',metavar='portnumber',type=int,nargs=1,help='port number for TCP connection')
+parser.add_argument('--retries',metavar='retries',type=int,nargs=1,default=[RETRIES],help='number of retries (default is %d)' % RETRIES)
+parser.add_argument('--timeout',metavar='timeout',type=int,nargs=1,default=[TIMEOUT_SECONDS],help='seconds to wait for TCP connection (default is %d). Along with --sleep, keep this to a sane high value.' % TIMEOUT_SECONDS)
+parser.add_argument('--sleep',metavar='sleep',type=int,nargs=1,default=[SLEEP_SECONDS],help='seconds to wait before retrying TCP connection (default is %d). Along with --timeout, keep this to a sane high value.' % SLEEP_SECONDS)
+args = parser.parse_args()
+
+hostname = args.hostname[0]
+port_number = args.port[0]
+retries = args.retries[0]
+timeout_seconds = args.timeout[0]
+sleep_seconds = args.sleep[0]
 
 
-USAGE="Usage: <hostname> <port>"
-SLEEP_SECONDS=5
-RETRIES=3
-TIMEOUT=5
-
-if (len(sys.argv) != 3):
-    print USAGE
-    quit(1)
-
-hostname = sys.argv[1]
-try:
-    port_number = int(sys.argv[2])
-except ValueError:
-  print USAGE
-  quit(1)
 try:
     server_address=socket.gethostbyname(hostname)
 except socket.error as ex:
-  sys.stderr.write("Could not resolve hostname!\n")
-  quit(1)
+    sys.stderr.write("Could not resolve hostname!\n")
+    quit(1)
 
 
-while(RETRIES):
+socketid = None
+
+
+while(retries):
     print "Trying to connect to %s - %s on port %d ..." % (hostname,server_address,port_number)
-    RETRIES=RETRIES-1
+    retries=retries-1
     try:
         socketid = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socketid.settimeout(TIMEOUT)
+        socketid.settimeout(timeout_seconds)
     except socket.error as ex:
         sys.stderr.write( "Could not create socket!\n")
+        sys.stderr.write(ex.message)
         quit(1)
+        
     try:
         socketid.connect((server_address,port_number))
     except socket.error as ex:
         print "Connect failed"
-        print "Sleeping %d seconds..." % SLEEP_SECONDS
-        time.sleep(SLEEP_SECONDS)
         socketid = None
-        continue
-    print "Connected."
+        if retries:
+            print "Sleeping %d seconds before retrying..." % sleep_seconds
+            time.sleep(sleep_seconds)
+
+        continue    
+    print "Connected!"
     socketid.close()
     socketid = None
-    break
+    quit(0)
 
 
 
