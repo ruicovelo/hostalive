9c2886fbf39165e8b0b69e4eee18089d8543758c
Rui Pedro Covelo <rui.covelo@gmail.com>
2015-01-19 17:40:48 +0000

    Adding keepalive feature. Reducing default retry interval to 10

--- hostalive.py
+++ hostalive.py
@@ -7,7 +7,7 @@ import argparse
 
 RETRIES=100
 TIMEOUT_SECONDS=10
-SLEEP_SECONDS=30
+SLEEP_SECONDS=10
 
 parser = argparse.ArgumentParser(description='Check if host/service is alive.')
 parser.add_argument('hostname',metavar='hostname',type=str,nargs=1,help='name of server (short or FQDN)')
@@ -15,6 +15,8 @@ parser.add_argument('port',metavar='portnumber',type=int,nargs=1,help='port numb
 parser.add_argument('--retries',metavar='retries',type=int,nargs=1,default=[RETRIES],help='number of retries (default is %d)' % RETRIES)
 parser.add_argument('--timeout',metavar='timeout',type=int,nargs=1,default=[TIMEOUT_SECONDS],help='seconds to wait for TCP connection (default is %d). Along with --sleep, keep this to a sane high value.' % TIMEOUT_SECONDS)
 parser.add_argument('--sleep',metavar='sleep',type=int,nargs=1,default=[SLEEP_SECONDS],help='seconds to wait before retrying TCP connection (default is %d). Along with --timeout, keep this to a sane high value.' % SLEEP_SECONDS)
+parser.add_argument('--keepalive',dest='keepalive',action='store_true',help='will continue trying connections even after a successful connection.')
+parser.set_defaults(keepalive=False)
 args = parser.parse_args()
 
 hostname = args.hostname[0]
@@ -22,7 +24,7 @@ port_number = args.port[0]
 retries = args.retries[0]
 timeout_seconds = args.timeout[0]
 sleep_seconds = args.sleep[0]
-
+keepalive = args.keepalive
 
 try:
     server_address=socket.gethostbyname(hostname)
@@ -44,22 +46,19 @@ while(retries):
         sys.stderr.write( "Could not create socket!\n")
         sys.stderr.write(ex.message)
         quit(1)
-        
+
     try:
         socketid.connect((server_address,port_number))
+        print "Connected!"
+        socketid.close()
     except socket.error as ex:
         print "Connect failed"
         socketid = None
-        if retries:
-            print "Sleeping %d seconds before retrying..." % sleep_seconds
-            time.sleep(sleep_seconds)
-
-        continue    
-    print "Connected!"
-    socketid.close()
+    if (keepalive and retries) or (socketid == None and retries):
+        print "Sleeping %d seconds before retrying..." % sleep_seconds
+        time.sleep(sleep_seconds)
+        continue
     socketid = None
     quit(0)
 
 quit(1)
-
-
